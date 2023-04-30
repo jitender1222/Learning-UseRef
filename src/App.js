5ebb41cd2e13e310ea10b3d9da9b30da21599803
jitender singh <jeetusinghkanyal@gmail.com>
2023-05-01 01:44:11 +0530

    learning use Ref

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+// import "./App.css";
 
-function App() {
+// import { useRef } from "react";
+
+// export default function App() {
+//   const countRef = useRef(0);
+//   console.log("count", countRef.current);
+//   const handle = () => {
+//     countRef.current++;
+//     console.log(`Clicked ${countRef.current} times`);
+//   };
+
+//   console.log("I rendered!");
+
+//   return <button onClick={handle}>Click me</button>;
+// }
+
+import { useRef, useState, useEffect } from "react";
+
+export default function App() {
+  const timerIdRef = useRef(0);
+  const [count, setCount] = useState(0);
+
+  const startHandler = () => {
+    if (timerIdRef.current) {
+      return;
+    }
+    timerIdRef.current = setInterval(() => setCount((c) => c + 1), 1000);
+  };
+
+  const stopHandler = () => {
+    clearInterval(timerIdRef.current);
+    timerIdRef.current = 0;
+  };
+
+  useEffect(() => {
+    return () => clearInterval(timerIdRef.current);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>Timer: {count}s</div>
+      <div>
+        <button onClick={startHandler}>Start</button>
+        <button onClick={stopHandler}>Stop</button>
+      </div>
     </div>
   );
 }
-
-export default App;
